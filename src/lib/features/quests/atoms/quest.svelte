122495d62651d27b18e66fdd055ea3a9157c1251
doninj <jean-baptiste.38@hotmail.com>
2023-07-03 09:52:18 +0200

    feat: ajout de la page quest

---
+++ src/lib/features/quests/atoms/quest.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { IQuest } from '$lib/models/quest'
+	import ProgressBar from './progressBar.svelte'
+
+	export let quest: IQuest
+</script>
+
+<div class=" w-[30%] px-10 mx-3 mt-5 bg-white border border-gray-200 rounded-lg shadow">
+	<div class="flex flex-col justify-center text-center items-center pb-10">
+		<img class="mt-8 w-1/4" alt="" src={quest.badge} />
+		<h5 class="mb-8 text-md font-medium text-gray-900">
+			{quest.name}
+		</h5>
+		<span class="text-xs text-center mb-8 mx-auto font-bold text-gray-700">{quest.description}</span
+		>
+		<ProgressBar progress="50" size="h-1.5" />
+		<div class="justify-center text-xs mt-5 text-center">600/700</div>
+	</div>
+</div>
--- src/routes/workspaces/[workspace]/quests/+page.svelte
+++ src/routes/workspaces/[workspace]/quests/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import badgeIllustration from '$lib/assets/badge.png'
 	import WithHeaderLayout from '$lib/components/layouts/WithHeaderLayout.svelte'
+	import Quest from '$lib/features/quests/atoms/quest.svelte'
+	import type { IQuest } from '$lib/models/quest'
+
+	const quest: IQuest = {
+		name: 'Sprint Parfait',
+		description: "Terminer toutes les tâches assignées lors d'un sprint sans aucun retard.",
+		badge: badgeIllustration
+	}
+	const listZob: Array<IQuest> = [quest, quest, quest, quest, quest, quest, quest]
 </script>
 
 <WithHeaderLayout
@@ -8,5 +17,9 @@
 	title="Quêtes"
 	subtitle="Accomplissez des quêtes de gestion de projet pour débloquer des récompenses et des niveaux de quête, et boostez votre productivité !"
 >
-	this is where my trophies should be displayed if I had any !!!!!
+	<div class="flex flex-1 flex-wrap">
+		{#each listZob as quest}
+			<Quest {quest} />
+		{/each}
+	</div>
 </WithHeaderLayout>
