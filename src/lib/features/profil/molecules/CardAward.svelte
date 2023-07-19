cb91881f6984868c46a26981283c67e31f0141e7
doninj <jean-baptiste.38@hotmail.com>
2023-07-19 23:02:18 +0200

    feat: ajout de la page des quetes débloquer

---
+++ src/lib/features/profil/molecules/CardAward.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { IQuest } from '$lib/models/quest'
+	export let award: IQuest
+</script>
+
+<div class="flex bg-white w-1/3 xl:w-[30%] min-h-12 mr-5 px-5 py-5 my-5 rounded-lg shadow-md">
+	<img src={award.badge} alt={award.name} class="w-20 h-20 mx-3" />
+	<div class="flex flex-col">
+		<p class="text-md mb-2 font-bold">{award.name}</p>
+		<p class="text-xs mb-2 text-start text-gray-400">{award.description}</p>
+		<p class="text-xs text-start text-gray-300">Débloqué le {award.completed_at}</p>
+	</div>
+</div>
--- src/lib/features/profil/organisms/QuestProfilContent.svelte
+++ src/lib/features/profil/organisms/QuestProfilContent.svelte
@@ -1,3 +1,10 @@
-<div>
-	<h1>Profil</h1>
+<script lang="ts">
+	import { questStore } from '$lib/stores/quest'
+	import CardAward from '../molecules/CardAward.svelte'
+</script>
+
+<div class="flex flex-1 mx-5 flex-wrap w-full">
+	{#each $questStore.quests as award}
+		<CardAward {award} />
+	{/each}
 </div>
