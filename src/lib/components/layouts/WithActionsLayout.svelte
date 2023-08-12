020ad91246b58de267cff3308ecd6d14ff713275
doninj <jean-baptiste.38@hotmail.com>
2023-08-12 16:12:10 +0200

    fix: prb props

--- src/lib/components/layouts/WithActionsLayout.svelte
+++ src/lib/components/layouts/WithActionsLayout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import Avatar from '$lib/features/settings/atoms/Avatar.svelte'
 	import { twMerge } from "tailwind-merge"
-	import type { IUser } from '$lib/models/auth'
-	import { currentWorkspace } from '$lib/stores/workspace'
-	export let user: IUser
+	export let name = ""
 </script>
 
 <div class={twMerge("flex h-[calc(100%-4rem)] bg-white", $$props.class)}>
@@ -12,7 +10,7 @@
 			<div class="w-full flex">
 				<div class="flex-1" />
 				<div class="w-[11%] h-32 mx-auto">
-					<Avatar name={$currentWorkspace.name} class="mt-8" />
+					<Avatar name={name} class="mt-8" />
 				</div>
 				<div class="flex-1 flex w-full justify-center items-end">
 					<slot name="actions" />
